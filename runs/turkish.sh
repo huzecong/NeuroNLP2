d971a5c3b497a984ce7131749ffc546bc740a52c
Zecong Hu <huzecong@gmail.com>
2017-11-29 20:13:45 -0500

    Add: all testing scripts

--- runs/turkish.sh
+++ runs/turkish.sh
@@ -5,9 +5,9 @@ python examples/GraphParser.py --mode FastLSTM --num_epochs 200 --batch_size 32 
  --objective cross_entropy --learning_rate 0.02 --decay_rate 0.05 --schedule 1 --gamma 0.0 \
  --p_in 0.33 --p_rnn 0.33 0.33 --p_out 0.33 --unk_replace 0.5 \
  --decode mst \
- --word_embedding polyglot --word_path "data/polyglot/polyglot-ar.pkl" --char_embedding random \
+ --word_embedding polyglot --word_path "data/polyglot/polyglot-tr.pkl" --char_embedding random \
  --punctuation 'G' 'X' \
- --train "data/conll2006/Arabic/arabic_PADT_train.conll" \
- --dev "data/conll2006/Arabic/arabic_PADT_dev.conll" \
- --test "data/conll2006/Arabic/arabic_PADT_test.conll" \
- --data_name "arabic_conll2006"
+ --train "data/conll2006/Turkish/turkish_metu_sabanci_train.conll" \
+ --dev "data/conll2006/Turkish/turkish_metu_sabanci_dev.conll" \
+ --test "data/conll2006/Turkish/turkish_metu_sabanci_test.conll" \
+ --data_name "turkish_conll2006"
