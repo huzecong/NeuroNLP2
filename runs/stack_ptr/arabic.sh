b711faf0ac94aed83cba74d6b03b98683e7239c6
Kanari <hzc14@mails.tsinghua.edu.cn>
2018-01-31 13:26:43 +0800

    Correct run scripts

--- runs/stack_ptr/arabic.sh
+++ runs/stack_ptr/arabic.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
+cat $0
 CUDA_VISIBLE_DEVICES=2 python examples/StackPointerParser.py --mode FastLSTM --num_epochs 1000 --batch_size 32 --decoder_input_size 256 --hidden_size 512 --encoder_layers 3 --decoder_layers 1 \
  --pos_dim 100 --char_dim 100 --num_filters 100 --arc_space 512 --type_space 128 \
  --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --schedule 10 --coverage 0.0 --gamma 0.0 --clip 5.0 \
  --p_in 0.33 --p_out 0.33 --p_rnn 0.33 0.33 --unk_replace 0.5 --label_smooth 1.0 --pos --beam 1 --prior_order inside_out \
  --skipConnect --grandPar --sibling \
- --word_embedding sskip --word_path "data/sskip/sskip.eng.100.gz" --char_embedding random \
- --punctuation '.' '``' "''" ':' ',' \
- --train "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.train.conll" \
- --dev "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.dev.conll" \
- --test "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.test.conll" \
- --model_path "models/parsing/stack_ptr/" --model_name 'network.pt'
+ --word_embedding polyglot --word_path "data/polyglot/polyglot-ar.pkl" --char_embedding random \
+ --punctuation 'G' 'X' \
+ --train "data/conll2006/Arabic/arabic_PADT_train.conll" \
+ --dev "data/conll2006/Arabic/arabic_PADT_dev.conll" \
+ --test "data/conll2006/Arabic/arabic_PADT_test.conll" \
+ --model_path "models/parsing/stack_ptr/" --model_name 'network.pt.arabic'
--- runs/stack_ptr/german.sh
+++ runs/stack_ptr/german.sh
@@ -4,9 +4,9 @@ CUDA_VISIBLE_DEVICES=2 python examples/StackPointerParser.py --mode FastLSTM --n
  --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --schedule 10 --coverage 0.0 --gamma 0.0 --clip 5.0 \
  --p_in 0.33 --p_out 0.33 --p_rnn 0.33 0.33 --unk_replace 0.5 --label_smooth 1.0 --pos --beam 1 --prior_order inside_out \
  --skipConnect --grandPar --sibling \
- --word_embedding sskip --word_path "data/sskip/sskip.eng.100.gz" --char_embedding random \
- --punctuation '.' '``' "''" ':' ',' \
- --train "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.train.conll" \
- --dev "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.dev.conll" \
- --test "data/PTB3.0/PTB3.0-Stanford_dep/ptb3.0-stanford.auto.cpos.test.conll" \
- --model_path "models/parsing/stack_ptr/" --model_name 'network.pt'
+ --word_embedding sskip --word_path "data/sskip/sskip.ger.64.gz" --char_embedding random \
+ --punctuation '$.' '$,' '$(' \
+ --train "data/conll2006/German/german_tiger_train.conll" \
+ --dev "data/conll2006/German/german_tiger_dev.conll" \
+ --test "data/conll2006/German/german_tiger_test.conll" \
+ --model_path "models/parsing/stack_ptr/" --model_name 'network.pt.german'
