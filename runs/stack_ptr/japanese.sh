5251f3a658afe860f6c8b846662c2279281b6b70
Kanari <hzc14@mails.tsinghua.edu.cn>
2018-02-03 11:11:32 +0800

    Remove --skipConnect; use BiAffine params for small datasets

--- runs/stack_ptr/japanese.sh
+++ runs/stack_ptr/japanese.sh
@@ -5,7 +5,7 @@ CUDA_VISIBLE_DEVICES=2 $PYTHON examples/StackPointerParser.py --mode FastLSTM --
  --pos_dim 100 --char_dim 100 --num_filters 100 --arc_space 512 --type_space 128 \
  --opt adam --learning_rate 0.001 --decay_rate 0.75 --epsilon 1e-4 --schedule 10 --coverage 0.0 --gamma 0.0 --clip 5.0 \
  --p_in 0.33 --p_out 0.33 --p_rnn 0.33 0.33 --unk_replace 0.5 --label_smooth 1.0 --pos --beam 1 --prior_order inside_out \
- --skipConnect --grandPar --sibling \
+ --grandPar --sibling \
  --word_embedding polyglot --word_path "data/polyglot/polyglot-ja-roman-naive.pkl" --char_embedding random \
  --punctuation '.' ',' '?' \
  --train "data/conll2006/Japanese/japanese_verbmobil_train.conll" \
